b8f22c5cd5084f4fb5ab35bbccd14173bfe273b0
sabrina <sabriiduarte@gmail.com>
2024-06-30 20:29:15 -0300

    tercer commit creamos instancia para darle valor al min, max e intentos y se llama al metodo jugar

--- src/adivinar_numero/Main.java
+++ src/adivinar_numero/Main.java
@@ -3,8 +3,13 @@ package adivinar_numero;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Creamos una instancia de juego con el mínimo = 1 y un máximo = 10 y 5 intentos disponibles 
+		AdivinarNum juego = new AdivinarNum(1,10,5);
+		
+		//Llama al método jugar() para iniciar el juego
+		juego.jugar();
+		
 	}
 
 }
